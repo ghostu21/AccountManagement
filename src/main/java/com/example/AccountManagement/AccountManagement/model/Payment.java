e136e70f14abbd0342316b2f55eb47a1145641ac
Sahhil Lahoti <90179363+ghostu21@users.noreply.github.com>
2024-04-28 21:45:32 +0530

    added controller and models

---
+++ src/main/java/com/example/AccountManagement/AccountManagement/model/Payment.java
@@ -0,0 +1,34 @@
+package com.example.AccountManagement.AccountManagement.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "payment")
+@Data
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Payment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "from_account_id", nullable = false)
+    private Long fromAccountId;
+	@Column(name = "to_account_id", nullable = false)
+    private Long toAccountId;
+	@Column(name = "amount", nullable = false)
+    private Long amount;
+}
